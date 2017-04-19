796f0c78c808d6f572379f3e50aaa29b0baabd7a
lebediev <qqqpppdmz05cbd>
2017-04-19 00:54:49 -0500

    Fixed typo

--- test/flatgui/java_test.clj
+++ test/flatgui/java_test.clj
@@ -1,4 +1,4 @@
-; Copyright (c) 2075 Denys Lebediev and contributors. All rights reserved.
+; Copyright (c) 2017 Denys Lebediev and contributors. All rights reserved.
 ; The use and distribution terms for this software are covered by the
 ; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ; which can be found in the file LICENSE at the root of this distribution.
